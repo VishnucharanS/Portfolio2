f60df5282116cdf6c16e08431052b08f628c7a1f
Vishnucharan <svc16122003@gmail.com>
2024-12-14 17:18:55 +0530

    eslint ignore

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "production": {
+            "rules": {
+                "no-unused-vars": "off"
+            }
+        }
+    },
+    "rules": {
+        "no-unused-vars": "warn" // Default for non-production environments
+    }
+};
