ed555e3e42ba9aea9944d8fe838d0073ff90c174
Vishnucharan <svc16122003@gmail.com>
2024-12-14 17:26:16 +0530

    eslint fixes

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,14 +1,17 @@
-import React, {useState, useEffect, lazy, Suspense} from "react";
+import React, {useState, useEffect, 
+  // lazy, 
+  // Suspense
+} from "react";
 import {openSource} from "../../portfolio";
 import Contact from "../contact/Contact";
-import Loading from "../loading/Loading";
+// import Loading from "../loading/Loading";
 
-const renderLoader = () => <Loading />;
-const GithubProfileCard = lazy(() =>
-  import("../../components/githubProfileCard/GithubProfileCard")
-);
+// const renderLoader = () => <Loading />;
+// const GithubProfileCard = lazy(() =>
+//   import("../../components/githubProfileCard/GithubProfileCard")
+// );
 export default function Profile() {
-  const [prof, setrepo] = useState([]);
+  const [_, setrepo] = useState([]);
   function setProfileFunction(array) {
     setrepo(array);
   }
